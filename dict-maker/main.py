3bf431308ab51e85a45c13542d36fc446f18379d
Alexander Slesarenko <dragongling@gmail.com>
2023-12-27 14:58:56 +0500

    stuff

--- dict-maker/main.py
+++ dict-maker/main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# from pymorphy2.opencorpora_dict import convert_to_pymorphy2
 
 
-# Press the green button in the gutter to run the script.
+def extract_cron_words(corpora_path, outfile):
+    needed_words_path = "words-we-need.txt"
+    needed_words = read_words_file(needed_words_path)
+    needed_words_regex = f"<l t=\"(?:" + "|".join(needed_words) + ")\">"
+    # needed_grammemes = ["Anum", "NUMR"]
+    common_regex = "(?:<g v=\"(Anum|NUMR)\"/>|" + needed_words_regex + ")"
+    with (open(corpora_path, 'r', encoding='utf-8') as file,
+          open(outfile, 'w', encoding='utf-8') as outfile):
+        for line in file:
+            if "<lemma " in line:
+                if re.search(common_regex, line):
+                    outfile.write(line)
+            elif "<link " not in line:
+                outfile.write(line)
+
+
+def read_words_file(needed_words_path):
+    needed_words = []
+    with open(needed_words_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            line = line.replace("\n", "")
+            if line != "":
+                needed_words.append(line)
+    return needed_words
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    needed_path = 'needed_corpora.xml'
+    dawg_path = 'dawg'
+    extract_cron_words('dict.opcorpora.xml', needed_path)
+    # convert_to_pymorphy2(needed_path, dawg_path, "hz", "ru")
